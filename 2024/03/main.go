99d94df54fa64b879bdea23c91a703e718073059
Kyle Cain <kylecain0305@gmail.com>
2024-12-03 17:48:05 -0500

    day 3 part 2

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -19,21 +19,36 @@ func main() {
 
     scanner := bufio.NewScanner(file)
 
-
     sum := 0
+    isAdding := true
+
     for scanner.Scan() {
         line := scanner.Text()
 
-        r := regexp.MustCompile(`mul\([0-9]{1,3},[0-9]{1,3}\)`)
+        r := regexp.MustCompile(`(do\(\))|(don't\(\))|(mul\([0-9]{1,3},[0-9]{1,3}\))`)
 
         matches := r.FindAllString(line, -1)
 
         for _, match := range matches {
+            if match == "do()" {
+                isAdding = true
+                continue
+            } else if match == "don't()" {
+                isAdding = false
+                continue
+            }
+
+            if !isAdding {
+                continue
+            }
+
             r1 := regexp.MustCompile(`[0-9]{1,3}`)
             m1 := r1.FindAllString(match, 2)
 
             x, _ := strconv.Atoi(m1[0])
             y, _ := strconv.Atoi(m1[1])
+
+            fmt.Println(isAdding, x, y)
 
             sum += x * y
         }
