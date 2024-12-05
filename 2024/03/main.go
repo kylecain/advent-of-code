1101351ee771e06484ec402420c6b56d70ce2a4c
Kyle Cain <kylecain0305@gmail.com>
2024-12-04 21:40:34 -0500

    lint

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -9,49 +9,49 @@ import (
 )
 
 func main() {
-    file, err := os.Open("input.txt")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    defer file.Close()
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 
-    sum := 0
-    isAdding := true
+	sum := 0
+	isAdding := true
 
-    for scanner.Scan() {
-        line := scanner.Text()
+	for scanner.Scan() {
+		line := scanner.Text()
 
-        r := regexp.MustCompile(`(do\(\))|(don't\(\))|(mul\([0-9]{1,3},[0-9]{1,3}\))`)
+		r := regexp.MustCompile(`(do\(\))|(don't\(\))|(mul\([0-9]{1,3},[0-9]{1,3}\))`)
 
-        matches := r.FindAllString(line, -1)
+		matches := r.FindAllString(line, -1)
 
-        for _, match := range matches {
-            if match == "do()" {
-                isAdding = true
-                continue
-            } else if match == "don't()" {
-                isAdding = false
-                continue
-            }
+		for _, match := range matches {
+			if match == "do()" {
+				isAdding = true
+				continue
+			} else if match == "don't()" {
+				isAdding = false
+				continue
+			}
 
-            if !isAdding {
-                continue
-            }
+			if !isAdding {
+				continue
+			}
 
-            r1 := regexp.MustCompile(`[0-9]{1,3}`)
-            m1 := r1.FindAllString(match, 2)
+			r1 := regexp.MustCompile(`[0-9]{1,3}`)
+			m1 := r1.FindAllString(match, 2)
 
-            x, _ := strconv.Atoi(m1[0])
-            y, _ := strconv.Atoi(m1[1])
+			x, _ := strconv.Atoi(m1[0])
+			y, _ := strconv.Atoi(m1[1])
 
-            fmt.Println(isAdding, x, y)
+			fmt.Println(isAdding, x, y)
 
-            sum += x * y
-        }
-    }
-        fmt.Println(sum)
+			sum += x * y
+		}
+	}
+	fmt.Println(sum)
 }
